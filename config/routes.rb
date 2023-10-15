2b6bfddc5ae5aec52f3a2e717a624a95af4cbb0f
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-15 07:28:32 +0000

    [add]認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :customers, controllers: {
+    sessions: 'customer/sessions',
+    registrations: 'customer/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "homes#top"
+  get '/up/', to: 'up#index', as: :up
+  get '/up/databases', to: 'up#databases', as: :up_databases
+
 end

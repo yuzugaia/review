24acbef6c8e10e20fb7c1b0cc7af4a7cb9c8dc89
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-17 05:46:43 +0000

    [add]sign_up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'products/index'
+    get 'products/show'
+    get 'products/new'
+    get 'products/edit'
+  end
   devise_for :admins, controllers: {
     sessions: 'admin/sessions'
   }
@@ -9,6 +15,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
+  namespace :admin do
+    resources :products, only: %i[index show new create edit update]
+  end
+  
   get '/up/', to: 'up#index', as: :up
   get '/up/databases', to: 'up#databases', as: :up_databases
 
